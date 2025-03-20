1c0f77092998297a9f60e4cda9f07c95575648fb
Tayná Martins Ramos <tayna.ramos@ifood.com.br>
2025-03-20 18:02:12 -0300

    Create selector to update balance

--- src/features/transactions/transactionsSlice.jsx
+++ src/features/transactions/transactionsSlice.jsx
@@ -4,29 +4,44 @@ const initialState = {
   transactions: [],
 };
 
-const transactionsSlice = createSlice({ 
-  name: "transactions", 
+const transactionsSlice = createSlice({
+  name: "transactions",
   initialState,
   reducers: {
     // list of actions
     addTransaction: (state, action) => {
+      let value = Math.abs(action.payload.value);
+
+      if (action.payload.type != "Depósito") {
+        value = -value;
+      }
+
       state.transactions.push({
         ...action.payload,
+        value,
         id: state.transactions.length + 1,
-        date: new Date().toISOString()
-      })
-    }
-  }
+        date: new Date().toISOString(),
+      });
+    },
+  },
 });
 
-export const selectTransactions = createSelector(state => state.transactions.transactions, (transactions) => {
-  return transactions.map(t => {
-    return {
-      ...t, 
-      date: new Date(t.date)
-    }
-  })
-})
+export const selectTransactions = createSelector(
+  (state) => state.transactions.transactions,
+  (transactions) => {
+    return transactions.map((t) => {
+      return {
+        ...t,
+        date: new Date(t.date),
+      };
+    });
+  }
+);
+
+export const selectCurrentBalance = createSelector(
+  selectTransactions,
+  (transaction) => transaction.reduce((balance, t) => balance + t.value, 0)
+);
 
 export const { addTransaction } = transactionsSlice.actions;
 
